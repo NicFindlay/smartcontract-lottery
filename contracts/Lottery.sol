3c95e6d07d5801f5eaa06049eb5c7a040b6d48bf
Nic <nicisme@gmail.com>
2022-07-14 14:55:08 +0200

    Contract Finished. Fulfil Randomness Chainlink

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -4,9 +4,12 @@ pragma solidity ^0.6.6;
 
 import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@chainlink/contracts/src/v0.6/VRFConsumerBase.sol";
 
-contract Lottery is Ownable {
+contract Lottery is VRFConsumerBase, Ownable {
     address payable[] public players;
+    address payable public recentWinner;
+    uint256 public randomness;
     uint256 public usdEntryFee;
     AggregatorV3Interface internal ethUsdPriceFeed;
     enum LOTTERY_STATE {
@@ -15,11 +18,21 @@ contract Lottery is Ownable {
         CALCULATING_WINNER
     }
     LOTTERY_STATE public lottery_state;
+    uint256 public fee;
+    bytes32 public keyhash;
 
-    constructor(address _priceFeedAddress) public {
+    constructor(
+        address _priceFeedAddress,
+        address _vrfCoordinator,
+        address _link,
+        uint256 _fee,
+        bytes32 _keyhash
+    ) public VRFConsumerBase(_vrfCoordinator, _link) {
         usdEntryFee = 50 * (10**18);
         ethUsdPriceFeed = AggregatorV3Interface(_priceFeedAddress);
         lottery_state = LOTTERY_STATE.CLOSED;
+        fee = _fee;
+        keyhash = _keyhash;
     }
 
     function enter() public payable {
@@ -32,7 +45,7 @@ contract Lottery is Ownable {
     function getEntranceFee() public view returns (uint256) {
         (, int256 price, , , ) = ethUsdPriceFeed.latestRoundData();
         uint256 adjustedPrice = uint256(price) * 10**10; //18 decimals
-        // $50, $2000 / Eth.   50/2000     50 * 100000 / 2000
+        // $50, $2000 / Eth.   50/2000     50 * 100000 / 2000lott
 
         uint256 costToEnter = (usdEntryFee * 10**18) / adjustedPrice;
         return costToEnter;
@@ -47,5 +60,32 @@ contract Lottery is Ownable {
     }
 
     //chainline VRF = Verifiable Randomness
-    function endLottery() public onlyOwner {}
+    function endLottery() public onlyOwner {
+        lottery_state = LOTTERY_STATE.CALCULATING_WINNER;
+        bytes32 requestID = requestRandomness(keyhash, fee);
+    }
+
+    function fulfillRandomness(bytes32 _requestId, uint256 _randomness)
+        internal
+        override
+    {
+        require(
+            lottery_state == LOTTERY_STATE.CALCULATING_WINNER,
+            "You aren't there yet!"
+        );
+        require(_randomness > 0, "random-not-found");
+        uint256 indexOfWinner = _randomness % players.length; //% is Mod, which returns what is leftover x / y
+        //7 Players, random number 22
+        // 22 % 7 = 1 (1 is leftover from below)
+        // 7 * 3 = 21
+        // 7 * 4 = 28
+
+        recentWinner = players[indexOfWinner];
+        recentWinner.transfer(address(this).balance);
+
+        //now reset
+        players = new address payable[](0);
+        lottery_state = LOTTERY_STATE.CLOSED;
+        randomness = _randomness;
+    }
 }
